22b6ed545cab50bc83757cf17308fff013faf656
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-03-21 12:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function holidayCountdown(holiday,days){
   return `It's ${days} days until ${holiday}!`;
 }
 happyHolidays(you);
-happyHolidaysTo(holiday, you)
+happyHolidaysTo(holiday, you);
 happyHolidaysTo("Mother's Day" , you);
 holidayCountdown("Mother's Day", 20);
